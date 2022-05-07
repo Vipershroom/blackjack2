70aa75f9f77170e4da43ec59886de99b8afa02c4
Vipershroom <undertalefan812@gmail.com>
2022-05-07 14:52:11 -0400

    Add game function

--- game.py
+++ game.py
@@ -1,4 +1,9 @@
 import random
+import os
+
 
 def drawCard():
     return random.randint(1,21)
+
+def game(playerList):
+    print(playerList)
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from Person import Person
+from game import game
+
 
 def getPlayers():
     print("How many players?")
@@ -9,7 +11,12 @@ def getPlayers():
         print(f"What is player {i + 1}'s name")
         name = input()
         playerList.append(Person(name, []))
+    
+    if amount == 1:
+        print("You will be facing the dealer")
+    
     return playerList
 
 if __name__ == "__main__":
     print("Welcome to Blackjack")
+    game(getPlayers())
